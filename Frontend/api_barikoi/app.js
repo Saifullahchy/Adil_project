bcf275091bc1587c4caf59ea1b9f166f1ea1612e
Saifullahchy <saifullahchy0177@gmail.com>
2022-01-22 16:59:20 +0600

    landing page v2 and popup window added

--- Frontend/api_barikoi/app.js
+++ Frontend/api_barikoi/app.js
@@ -8,7 +8,16 @@ const distance =  fetch(`https://barikoi.xyz/v1/api/distance/${API_KEY}/${SOURCE
     .catch(error => console.error('Error:', error))
     .then(response => console.log('Success:', response))
 
-console.log(distance);
+// document.getElementById('distance').innerHTML = distance.Distance;
+
+// function appendData(data) {
+//   var mainContainer = document.getElementById("distance");
+//   for (var i = 0; i < data.length; i++) {
+//     var h4 = document.createElement("h4");
+//     h4.innerHTML = 'Distance: ' + data[i].Distance;
+//     mainContainer.appendChild(div);
+//   }
+// }
 
 const Start = fetch(`https://barikoi.xyz/v1/api/search/reverse/geocode/server/${API_KEY}/place?longitude=${SOURCE_LONGITUDE}&latitude=${SOURCE_LATITUDE}&district=true&post_code=true&country=true&sub_district=true&union=true&pauroshova=true&location_type=true&division=true&address=true&area=true`)
 .then(response => response.json())
@@ -19,3 +28,26 @@ const END = fetch(`https://barikoi.xyz/v1/api/search/reverse/geocode/server/${AP
 .then(response => response.json())
 .catch(error => console.error('Error:', error))
 .then(response => console.log('Success:', response))
+
+// let data = {
+//     "api_key": API_KEY,
+//     "source": "23.746086,90.37368",
+//     "destination": "23.746214,90.371654",
+//     "profile": "car",
+//     "geo_points": [
+//         {"id": 1, "point": "23.746086,90.37368"},
+//         {"id": 2, "point": "23.74577,90.373389"},
+//         {"id": 3, "point": "23.74442,90.372909"},
+//         {"id": 4, "point": "23.743961,90.37214"}
+//       ]
+//    };
+//       fetch("http://barikoi.xyz/v2/api/route/optimized",
+//     {
+//        headers: {
+//         'Content-Type': 'application/json'
+//        },
+//         method: "POST",
+//         body: JSON.stringify(data)
+//     })
+//     .then(response => response.json())
+//     .catch(error => console.error('Error:', error))
