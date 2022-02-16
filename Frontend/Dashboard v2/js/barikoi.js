60ccd8f0553431bf8e8f4aa465bcaaf3e6cc22da
Saifullahchy <saifullahchy0177@gmail.com>
2022-02-16 13:49:43 +0600

    start and end location and end location added

--- Frontend/Dashboard v2/js/barikoi.js
+++ Frontend/Dashboard v2/js/barikoi.js
@@ -1,17 +1,17 @@
 const api_KEY = 'Mjc4OTpSSlJZMlZVNlE3';
 
+// 23.76244773181037, 90.38476808411826
+const currentMapLon =  90.38476808411826;
+const currentMapLat = 23.76244773181037;
 
-const currentMapLon = 90.40581243181647;
-const currentMapLat = 23.778169136666268;
-
-// 23.778169136666268, 90.40581243181647 mohakhali
+//23.77368000882191, 90.42446233993964 mohakhali
 // 23.762155442445902, 90.384927483916 accigone office
 // 23.762120056279528, 90.38494288035577
 
-const startTripLon = "";
-const startTripLat = "";
-const endTripLon = 90.38494288035577;
-const endTripLat = 23.762120056279528;
+const startTripLon = 90.38494288035577;
+const startTripLat = 23.762120056279528;
+const endTripLon = 90.42446233993964;
+const endTripLat = 23.77368000882191;
 // 23.777824599341365, 90.40571256931142
 
 const currentMapLoc = document.getElementById('currentLocation');
@@ -20,8 +20,10 @@ const currentVehicleLocation = document.getElementById('current_vehicle_location
 const endLocation = document.getElementById('End-Location');
 
 const reverseGeoApiUrl = `https://barikoi.xyz/v1/api/search/reverse/${api_KEY}/geocode?longitude=${currentMapLon}&latitude=${currentMapLat}&district=true&post_code=true&country=true&sub_district=true&union=true&pauroshova=true&location_type=true&division=true&address=true&area=true`;
-
+const startGeoApiUrl = `https://barikoi.xyz/v1/api/search/reverse/${api_KEY}/geocode?longitude=${startTripLon}&latitude=${startTripLat}&district=true&post_code=true&country=true&sub_district=true&union=true&pauroshova=true&location_type=true&division=true&address=true&area=true`;
 const reverseGeoApiUrlEndPosition = `https://barikoi.xyz/v1/api/search/reverse/${api_KEY}/geocode?longitude=${endTripLon}&latitude=${endTripLat}&district=true&post_code=true&country=true&sub_district=true&union=true&pauroshova=true&location_type=true&division=true&address=true&area=true`
+
+
 const makeApiRequest = async (url) =>{
     const res = await fetch(url);
     if(!res.ok){
@@ -37,11 +39,18 @@ const getAddress = () => {
     makeApiRequest(reverseGeoApiUrl).then((res) => {
         console.log(res);
         let address = res;
-       currentMapLoc.innerHTML = address.place.address;
+       
        currentVehicleLocation.innerHTML = address.place.address;
 
     });
 };
+
+const startLocation  = () => {
+    makeApiRequest(reverseGeoApiUrl).then((res) => {
+        let startLoc = res;
+        currentMapLoc.innerHTML = startLoc.place.address;
+    })
+}
 
 const endAddress = () => {
     makeApiRequest(reverseGeoApiUrlEndPosition).then((res) => {
@@ -52,6 +61,7 @@ const endAddress = () => {
 }
 
 getAddress();
+startLocation();
 endAddress();
 
 
